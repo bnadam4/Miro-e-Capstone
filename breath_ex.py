2a9877a81c2babb7fa620a24739caccc8bc97ff4
d4ma <49056111+d4ma@users.noreply.github.com>
2024-10-04 17:35:09 -0700

    Updated breath_ex.py with instruction audios

--- breath_ex.py
+++ breath_ex.py
@@ -10,13 +10,27 @@
 import time
 import math
 import os
+import sys
 import numpy as np
 
 # Robot specific libraries
 import rospy
-from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import Float32MultiArray, Int16MultiArray, String, UInt16MultiArray
 from sensor_msgs.msg import JointState
 import miro2 as miro
+
+# messages larger than this will be dropped by the receiver
+MAX_STREAM_MSG_SIZE = (4096 - 48)
+
+# amount to keep the buffer stuffed - larger numbers mean
+# less prone to dropout, but higher latency when we stop
+# streaming. with a read-out rate of 8k, 2000 samples will
+# buffer for quarter of a second, for instance.
+BUFFER_STUFF_BYTES = 4000
+
+def error(msg):
+	print(msg)
+	sys.exit(0)
 
 # Generate enum and constants for the breathing exercise states and joints
 breath_in, hold_1, breath_out, hold_2 = range(4)
@@ -27,12 +41,60 @@ neck_upper = 15.0 # deg
 neck_lower = 40.0 # deg
 state_duration = 4.0
 
+TRACK_FILE = 'sunflower.mp3'
+TRACK_PATH= f'../../../share/media/{TRACK_FILE}'
+
+
+# Audio file paths for different states
+AUDIO_FILES = {
+    "breathe_in": 'alice_breathe_in.mp3',
+    "breathe_out": 'alice_breathe_out.mp3',
+    "hold_1": 'alice_hold.mp3',
+    "hold_2": 'alice_hold.mp3'
+}
+
 class breath_ex:
 
     TICK = 0.02 # Update interval for the main controller loop in seconds
 
-    def __init__(self):
+    def callback_log(self, msg):
+        sys.stdout.write(msg.data)
+        sys.stdout.flush()
 
+    def callback_stream(self, msg):
+        self.buffer_space = msg.data[0]
+        self.buffer_total = msg.data[1]
+
+    def play_audio(self, track_file):
+        decoded_file_path = f"../../../share/media/{track_file}.decode"
+        
+        if not os.path.isfile(decoded_file_path):
+            print(f"Error: No decoded file found at {decoded_file_path}.")
+            print("Make sure to run ../../../share/media/decode_audio.py before trying again.")
+            return
+        
+        try:
+            with open(decoded_file_path, 'rb') as f:
+                dat = f.read()
+                print(f"Read {len(dat)} bytes from {decoded_file_path}")
+        except Exception as e:
+            print(f"Error while reading {decoded_file_path}: {e}")
+            return
+        
+        self.data_r = 0
+        dat = np.fromstring(dat, dtype='int16').astype(np.int32)
+        dat = dat.astype(np.float)
+        sc = 32767.0 / np.max(np.abs(dat))
+        dat *= sc
+        dat = dat.astype(np.int16).tolist()
+        self.data = dat
+
+        print(f"Playing {track_file}")
+        # Reset buffer space for streaming
+        self.buffer_space = 0
+        self.buffer_total = 0
+
+    def __init__(self):
         self.neck_speed = (neck_lower - neck_upper)/(state_duration/self.TICK)
         self.eyelid_speed = 1.0/(state_duration/self.TICK)
         self.state = breath_in
@@ -40,6 +102,11 @@ class breath_ex:
         self.current_time = time.time()
         self.neck_pos = 40.0
         self.eyelid_pos = 0.0
+
+        #load decoded audio files into memory
+        # load wav
+        # Check if the decoded file exists
+        # Construct the full path of the decoded file
 
         # robot name
         topic_base_name = "/" + os.getenv("MIRO_ROBOT_NAME")
@@ -53,6 +120,12 @@ class breath_ex:
         # Publishers
         self.pub_kin = rospy.Publisher(topic_base_name + "/control/kinematic_joints", JointState, queue_size=0)
         self.pub_cos = rospy.Publisher(topic_base_name + "/control/cosmetic_joints", Float32MultiArray, queue_size=0)
+        self.pub_stream = rospy.Publisher(topic_base_name + "/control/stream", Int16MultiArray, queue_size=0)
+
+        #subscribe
+        self.sub_log = rospy.Subscriber(topic_base_name + "/platform/log", String, self.callback_log, queue_size=5, tcp_nodelay=True)
+        self.sub_stream = rospy.Subscriber(topic_base_name + "/sensors/stream", UInt16MultiArray, self.callback_stream, queue_size=1, tcp_nodelay=True)
+
 
         # Set up variables for the kinetic joints
         self.kin_joints = JointState()
@@ -72,8 +145,16 @@ class breath_ex:
         Main control loop
         """
 
+        # periodic reports
+        count = 0
+
+        # safety dropout if receiver not present
+        dropout_data_r = -1
+        dropout_count = 3
+
         # Main control loop iteration counter
         self.counter = 0
+        self.last_state = None
 
         print("MiRo does some deep breathing")
 
@@ -90,7 +171,21 @@ class breath_ex:
                     self.state = hold_2
                 elif self.state == hold_2:
                     self.state = breath_in
-                
+
+            # Check if state has changed
+            if self.state != self.last_state:
+                # Play the appropriate audio for the current state
+                if self.state == breath_in:
+                    self.play_audio(AUDIO_FILES["breathe_in"])
+                elif self.state == hold_1:
+                    self.play_audio(AUDIO_FILES["hold_1"])
+                elif self.state == breath_out:
+                    self.play_audio(AUDIO_FILES["breathe_out"])
+                elif self.state == hold_2:
+                    self.play_audio(AUDIO_FILES["hold_2"])
+
+                # Update last_state to the current state
+                self.last_state = self.state
 
             if self.state == breath_in:
                  # Make it look like MiRo is breathing in
@@ -119,10 +214,54 @@ class breath_ex:
 
             self.cos_joints.data[left_eye] = self.eyelid_pos
             self.cos_joints.data[right_eye] = self.eyelid_pos
-            print("eyelid_pos = ", self.eyelid_pos)
+            # print("eyelid_pos = ", self.eyelid_pos)
              
             self.pub_kin.publish(self.kin_joints)
             self.pub_cos.publish(self.cos_joints)
+
+
+            # audio stream
+
+            # if we've received a report
+            if self.buffer_total > 0:
+
+                # compute amount to send
+                buffer_rem = self.buffer_total - self.buffer_space
+                n_bytes = BUFFER_STUFF_BYTES - buffer_rem
+                n_bytes = max(n_bytes, 0)
+                n_bytes = min(n_bytes, MAX_STREAM_MSG_SIZE)
+
+                # if amount to send is non-zero
+                if n_bytes > 0:
+
+                    msg = Int16MultiArray(data = self.data[self.data_r:self.data_r+n_bytes])
+                    self.pub_stream.publish(msg)
+                    self.data_r += n_bytes
+
+            # break
+            if self.data_r <= len(self.data):
+                # report once per second
+                if count == 0:
+                    count = 10
+                    print ("streaming:", self.data_r, "/", len(self.data), "bytes")
+
+                    # check at those moments if we are making progress, also
+                    if dropout_data_r == self.data_r:
+                        if dropout_count == 0:
+                            print ("dropping out because of no progress...")
+                            break
+                        print ("dropping out in", str(dropout_count) + "...")
+                        dropout_count -= 1
+                    else:
+                        dropout_data_r = self.data_r
+
+                # count tenths
+                count -= 1
+            else:
+                print("audio playback finished")
+
+            # end audio stream
+            
 
             # Yield
             rospy.sleep(self.TICK)
