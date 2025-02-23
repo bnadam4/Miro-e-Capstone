fc0e190ee916a4034ccce6caba1a92594e18038a
Jimmy Wang <jwang239@hotmail.com>
2025-02-23 12:07:08 -0800

    relaxation changes

--- robot_project/behaviors/relax.py
+++ robot_project/behaviors/relax.py
@@ -25,6 +25,8 @@ from actuators.play_audio import play_audio  # function
 from IS_modules.node_detect_aruco import *
 from IS_modules.detect_touch import *
 
+from actuators.speech_to_text import SpeechToText
+
 class RelaxBehavior:
     def __init__(self):
         # Initialize all actuators
@@ -35,118 +37,140 @@ class RelaxBehavior:
         self.cosmetics_movement = CosmeticsMovement()
         #self.robot_interface = RobotInterface()
         self.state = "relax_prep"
+        self.timers = []  # List to track timers
+        self.flag = None  # Initialize flag variable
         
         speech_to_text_thread = threading.Thread(target=self.speech_to_text.loop)
         speech_to_text_thread.daemon = True
         speech_to_text_thread.start()
 
-    def run(self):
-        while self.state != "complete":
-            if self.state == "relax_prep":
-                self.relax_prep()
-            elif self.state == "relax_back":
-                self.relax_back()
-            elif self.state == "relax_arms":
-                self.relax_arms()
-            elif self.state == "relax_tummy":
-                self.relax_tummy()
-            elif self.state == "relax_legs":
-                self.relax_legs()
-            elif self.state == "relax_complete":
-                self.relax_complete()
+    def check_exit_flag(self):
+        while not self.stop_flag:
+            # Detect aruco markers
+            self.aruco_detect.tick_camera()
+            if self.aruco_detect.exit_behaviour:
+                self.stop_flag = True
+                self.audio_player.stop()
+                print("[RELAXATION] Exit behaviour detected, stopping audiobook.")
+            time.sleep(0.1)
 
-    def relax_prep(self):
-        audio_file = 'relax_prep.mp3'
+    def safe_execute(self, func, args):
+        """Execute function only if stop flag is not set and parent thread is still alive."""
+        if not self.stop_flag and (self.parent_thread and self.parent_thread.is_alive()):
+            func(*args)
+        else:
+            print(f"[RELAXATION] Skipping {func.__name__} because parent thread has exited.")
+
+    # Function to safely start actions
+    def add_timer(self, delay, func, args=()):
+        timer = threading.Timer(delay, lambda: self.safe_execute(func, args))
+        self.timers.append(timer)
+        timer.start()
+
+    def run(self, flag=None):
+        if flag == "relax_back":
+            self.relax_back()
+        elif flag == "relax_arms":
+            self.relax_arms()
+        elif flag == "relax_tummy":
+            self.relax_tummy()
+        elif flag == "relax_legs":
+            self.relax_legs()
+        elif flag == "relax_full":
+            self.relax_back()
+            self.relax_arms()
+            self.relax_tummy()
+            self.relax_legs()
+        else:
+            self.exit()
+
+    def add_timer(self, delay, func, args=()):
+        timer = threading.Timer(delay, lambda: func(*args))
+        self.timers.append(timer)
+        timer.start()
+
+    def relax_begin(self):
+        audio_file = 'relax_begin.mp3'
         play_thread = threading.Thread(target=play_audio, args=(audio_file,))
         play_thread.start()
 
-        # Run ear movement from 0-2s
-        time.sleep(1)
-        head_thread = threading.Thread(target=self.joints_movement.nod, args=(2,2))
-        head_thread.start()
-
-        # Turn on LED from 1-8s
-        time.sleep(3)
-        led_thread_1 = threading.Thread(target=self.led_controller.toggle_led_sections, args=(7, (255, 0, 0),(0, 255, 0),(0, 0, 255), 250))
-        led_thread_1.start()
-
-        # Blink eyes at 10s
-        time.sleep(5)
-        eyes_thread = threading.Thread(target=self.cosmetics_movement.close_eyes, args=(1,))
-        eyes_thread.start()
-
-        time.sleep(7)
-        eyes_open_thread = threading.Thread(target=self.cosmetics_movement.open_eyes, args=(1,))
-        eyes_open_thread.start()
-
-        time.sleep(9)
-        head_tilt_thread = threading.Thread(target=self.joints_controller.move_neck, args=(2, 35))
-        head_tilt_thread.start()
-
-        time.sleep(11)
-        head_tilt_thread = threading.Thread(target=self.joints_controller.move_neck, args=(2, 0))
-        head_tilt_thread.start()
-
-        time.sleep(12)
-        eyes_thread = threading.Thread(target=self.cosmetics_movement.close_eyes, args=(1,))
-        eyes_thread.start()
-
-        time.sleep(13)
-        eyes_open_thread = threading.Thread(target=self.cosmetics_movement.open_eyes, args=(1,))
-        eyes_open_thread.start()
+        # Schedule all movements
+        self.add_timer(1, self.joints_movement.nod, (2, 2))
+        self.add_timer(4, self.led_controller.toggle_led_sections, (7, (255, 0, 0), (0, 255, 0), (0, 0, 255), 250))
+        self.add_timer(9, self.cosmetics_movement.close_eyes, (1,))
+        self.add_timer(10, self.cosmetics_movement.open_eyes, (1,))
+        self.add_timer(12, self.joints_controller.move_neck, (2, 35))
+        self.add_timer(14, self.joints_controller.move_neck, (2, 0))
+        self.add_timer(15, self.cosmetics_movement.close_eyes, (1,))
+        self.add_timer(16, self.cosmetics_movement.open_eyes, (1,))
 
         # Wait for the first set of actions to finish
-        head_thread.join()
-        led_thread_1.join()
-        eyes_thread.join()
-        eyes_open_thread.join()
         play_thread.join()
+        for timer in self.timers:
+            timer.join()
         time.sleep(5)
 
         self.state = "relax_back"
+    
+    def relax_prompt(self):
+        audio_file = 'relax_prompt.mp3'
+        play_thread = threading.Thread(target=play_audio, args=(audio_file,))
+        play_thread.start()
+
+        # Schedule all movements
+        self.add_timer(1, self.joints_movement.nod, (2, 2))
+        self.add_timer(4, self.led_controller.toggle_led_sections, (7, (255, 0, 0), (0, 255, 0), (0, 0, 255), 250))
+        self.add_timer(9, self.cosmetics_movement.close_eyes, (1,))
+        self.add_timer(10, self.cosmetics_movement.open_eyes, (1,))
+        self.add_timer(12, self.joints_controller.move_neck, (2, 35))
+        self.add_timer(14, self.joints_controller.move_neck, (2, 0))
+        self.add_timer(15, self.cosmetics_movement.close_eyes, (1,))
+        self.add_timer(16, self.cosmetics_movement.open_eyes, (1,))
+
+        # Wait for the first set of actions to finish
+        play_thread.join()
+        for timer in self.timers:
+            timer.join()
+        time.sleep(5)
+
+        full_check = ['full', 'everything', 'all']
+        back_check = ['back']
+        arms_check = ['arms']
+        tummy_check = ['tummy']
+        legs_check = ['legs']
+
+        if any(word in self.speech_to_text.last_text.lower() for word in full_check):
+            self.flag = "relax_full"
+        elif any(word in self.speech_to_text.last_text.lower() for word in back_check):
+            self.flag = "relax_back"
+        elif any(word in self.speech_to_text.last_text.lower() for word in arms_check):
+            self.flag = "relax_arms"
+        elif any(word in self.speech_to_text.last_text.lower() for word in tummy_check):
+            self.flag = "relax_tummy"
+        elif any(word in self.speech_to_text.last_text.lower() for word in legs_check):
+            self.flag = "relax_legs"
+        else:
+            self.flag = None
 
     def relax_back(self):
         audio_file = 'relax_back.mp3'
         play_thread = threading.Thread(target=play_audio, args=(audio_file,))
         play_thread.start()
 
-        time.sleep(5)
-        eyes_thread = threading.Thread(target=self.cosmetics_movement.close_eyes, args=(1,))
-        eyes_thread.start()
+        # Schedule all movements
+        self.add_timer(5, self.cosmetics_movement.close_eyes, (1,))
+        self.add_timer(6, self.cosmetics_movement.open_eyes, (1,))
+        self.add_timer(10, self.joints_movement.nod, (2, 2))
+        self.add_timer(16, self.led_controller.toggle_led_sections, (7, (255, 0, 0), (0, 255, 0), (0, 0, 255), 250))
+        self.add_timer(17, self.led_controller.toggle_led_sections, (7, (255, 0, 0), (0, 0, 0), (0, 0, 0), 250))
+        self.add_timer(18, self.led_controller.toggle_led_sections, (7, (0, 0, 0), (0, 255, 0), (0, 0, 0), 250))
+        self.add_timer(19, self.led_controller.toggle_led_sections, (7, (0, 0, 0), (0, 0, 0), (0, 0, 255), 250))
+        self.add_timer(20, self.led_controller.toggle_led_sections, (7, (100, 0, 0), (0, 100, 0), (0, 0, 100), 250))
 
-        time.sleep(1)
-        eyes_open_thread = threading.Thread(target=self.cosmetics_movement.open_eyes, args=(1,))
-        eyes_open_thread.start()
-
-        time.sleep(5)
-        head_thread = threading.Thread(target=self.joints_movement.nod, args=(2,2))
-        head_thread.start()
-
-        time.sleep(6)
-        led_thread_1 = threading.Thread(target=self.led_controller.toggle_led_sections, args=(7, (255, 0, 0),(0, 255, 0),(0, 0, 255), 250))
-        led_thread_1.start()
-
-        time.sleep(1)
-        led_thread_1 = threading.Thread(target=self.led_controller.toggle_led_sections, args=(7, (255, 0, 0),(0, 0, 0),(0, 0, 0), 250))
-        led_thread_1.start()
-
-        time.sleep(1)
-        led_thread_1 = threading.Thread(target=self.led_controller.toggle_led_sections, args=(7, (0, 0, 0),(0, 255, 0),(0, 0, 0), 250))
-        led_thread_1.start()
-
-        time.sleep(1)
-        led_thread_1 = threading.Thread(target=self.led_controller.toggle_led_sections, args=(7, (0, 0, 0),(0, 0, 0),(0, 0, 255), 250))
-        led_thread_1.start()
-
-        time.sleep(1)
-        led_thread_1 = threading.Thread(target=self.led_controller.toggle_led_sections, args=(7, (100, 0, 0),(0, 100, 0),(0, 0, 100), 250))
-        led_thread_1.start()
-
-        head_thread.join()
-        led_thread_1.join()
-        eyes_thread.join()
-        eyes_open_thread.join()
+        # Wait for the first set of actions to finish
         play_thread.join()
+        for timer in self.timers:
+            timer.join()
         time.sleep(5)
 
         self.state = "relax_arms"
@@ -156,43 +180,20 @@ class RelaxBehavior:
         play_thread = threading.Thread(target=play_audio, args=(audio_file,))
         play_thread.start()
 
-        time.sleep(5)
-        eyes_thread = threading.Thread(target=self.cosmetics_movement.close_eyes, args=(1,))
-        eyes_thread.start()
+        # Schedule all movements
+        self.add_timer(5, self.cosmetics_movement.close_eyes, (1,))
+        self.add_timer(6, self.cosmetics_movement.open_eyes, (1,))
+        self.add_timer(10, self.joints_movement.nod, (2, 2))
+        self.add_timer(16, self.led_controller.toggle_led_sections, (7, (255, 0, 0), (0, 255, 0), (0, 0, 255), 250))
+        self.add_timer(17, self.led_controller.toggle_led_sections, (7, (255, 0, 0), (0, 0, 0), (0, 0, 0), 250))
+        self.add_timer(18, self.led_controller.toggle_led_sections, (7, (0, 0, 0), (0, 255, 0), (0, 0, 0), 250))
+        self.add_timer(19, self.led_controller.toggle_led_sections, (7, (0, 0, 0), (0, 0, 0), (0, 0, 255), 250))
+        self.add_timer(20, self.led_controller.toggle_led_sections, (7, (100, 0, 0), (0, 100, 0), (0, 0, 100), 250))
 
-        time.sleep(1)
-        eyes_open_thread = threading.Thread(target=self.cosmetics_movement.open_eyes, args=(1,))
-        eyes_open_thread.start()
-
-        time.sleep(5)
-        head_thread = threading.Thread(target=self.joints_movement.nod, args=(2,2))
-        head_thread.start()
-
-        time.sleep(6)
-        led_thread_1 = threading.Thread(target=self.led_controller.toggle_led_sections, args=(7, (255, 0, 0),(0, 255, 0),(0, 0, 255), 250))
-        led_thread_1.start()
-
-        time.sleep(1)
-        led_thread_1 = threading.Thread(target=self.led_controller.toggle_led_sections, args=(7, (255, 0, 0),(0, 0, 0),(0, 0, 0), 250))
-        led_thread_1.start()
-
-        time.sleep(1)
-        led_thread_1 = threading.Thread(target=self.led_controller.toggle_led_sections, args=(7, (0, 0, 0),(0, 255, 0),(0, 0, 0), 250))
-        led_thread_1.start()
-
-        time.sleep(1)
-        led_thread_1 = threading.Thread(target=self.led_controller.toggle_led_sections, args=(7, (0, 0, 0),(0, 0, 0),(0, 0, 255), 250))
-        led_thread_1.start()
-
-        time.sleep(1)
-        led_thread_1 = threading.Thread(target=self.led_controller.toggle_led_sections, args=(7, (100, 0, 0),(0, 100, 0),(0, 0, 100), 250))
-        led_thread_1.start()
-
-        head_thread.join()
-        led_thread_1.join()
-        eyes_thread.join()
-        eyes_open_thread.join()
+        # Wait for the first set of actions to finish
         play_thread.join()
+        for timer in self.timers:
+            timer.join()
         time.sleep(5)
 
         self.state = "relax_tummy"
@@ -202,43 +203,20 @@ class RelaxBehavior:
         play_thread = threading.Thread(target=play_audio, args=(audio_file,))
         play_thread.start()
 
-        time.sleep(5)
-        eyes_thread = threading.Thread(target=self.cosmetics_movement.close_eyes, args=(1,))
-        eyes_thread.start()
+        # Schedule all movements
+        self.add_timer(5, self.cosmetics_movement.close_eyes, (1,))
+        self.add_timer(6, self.cosmetics_movement.open_eyes, (1,))
+        self.add_timer(10, self.joints_movement.nod, (2, 2))
+        self.add_timer(16, self.led_controller.toggle_led_sections, (7, (255, 0, 0), (0, 255, 0), (0, 0, 255), 250))
+        self.add_timer(17, self.led_controller.toggle_led_sections, (7, (255, 0, 0), (0, 0, 0), (0, 0, 0), 250))
+        self.add_timer(18, self.led_controller.toggle_led_sections, (7, (0, 0, 0), (0, 255, 0), (0, 0, 0), 250))
+        self.add_timer(19, self.led_controller.toggle_led_sections, (7, (0, 0, 0), (0, 0, 0), (0, 0, 255), 250))
+        self.add_timer(20, self.led_controller.toggle_led_sections, (7, (100, 0, 0), (0, 100, 0), (0, 0, 100), 250))
 
-        time.sleep(1)
-        eyes_open_thread = threading.Thread(target=self.cosmetics_movement.open_eyes, args=(1,))
-        eyes_open_thread.start()
-
-        time.sleep(5)
-        head_thread = threading.Thread(target=self.joints_movement.nod, args=(2,2))
-        head_thread.start()
-
-        time.sleep(6)
-        led_thread_1 = threading.Thread(target=self.led_controller.toggle_led_sections, args=(7, (255, 0, 0),(0, 255, 0),(0, 0, 255), 250))
-        led_thread_1.start()
-
-        time.sleep(1)
-        led_thread_1 = threading.Thread(target=self.led_controller.toggle_led_sections, args=(7, (255, 0, 0),(0, 0, 0),(0, 0, 0), 250))
-        led_thread_1.start()
-
-        time.sleep(1)
-        led_thread_1 = threading.Thread(target=self.led_controller.toggle_led_sections, args=(7, (0, 0, 0),(0, 255, 0),(0, 0, 0), 250))
-        led_thread_1.start()
-
-        time.sleep(1)
-        led_thread_1 = threading.Thread(target=self.led_controller.toggle_led_sections, args=(7, (0, 0, 0),(0, 0, 0),(0, 0, 255), 250))
-        led_thread_1.start()
-
-        time.sleep(1)
-        led_thread_1 = threading.Thread(target=self.led_controller.toggle_led_sections, args=(7, (100, 0, 0),(0, 100, 0),(0, 0, 100), 250))
-        led_thread_1.start()
-
-        head_thread.join()
-        led_thread_1.join()
-        eyes_thread.join()
-        eyes_open_thread.join()
+        # Wait for the first set of actions to finish
         play_thread.join()
+        for timer in self.timers:
+            timer.join()
         time.sleep(5)
 
         self.state = "relax_legs"
@@ -248,43 +226,20 @@ class RelaxBehavior:
         play_thread = threading.Thread(target=play_audio, args=(audio_file,))
         play_thread.start()
 
-        time.sleep(5)
-        eyes_thread = threading.Thread(target=self.cosmetics_movement.close_eyes, args=(1,))
-        eyes_thread.start()
+        # Schedule all movements
+        self.add_timer(5, self.cosmetics_movement.close_eyes, (1,))
+        self.add_timer(6, self.cosmetics_movement.open_eyes, (1,))
+        self.add_timer(10, self.joints_movement.nod, (2, 2))
+        self.add_timer(16, self.led_controller.toggle_led_sections, (7, (255, 0, 0), (0, 255, 0), (0, 0, 255), 250))
+        self.add_timer(17, self.led_controller.toggle_led_sections, (7, (255, 0, 0), (0, 0, 0), (0, 0, 0), 250))
+        self.add_timer(18, self.led_controller.toggle_led_sections, (7, (0, 0, 0), (0, 255, 0), (0, 0, 0), 250))
+        self.add_timer(19, self.led_controller.toggle_led_sections, (7, (0, 0, 0), (0, 0, 0), (0, 0, 255), 250))
+        self.add_timer(20, self.led_controller.toggle_led_sections, (7, (100, 0, 0), (0, 100, 0), (0, 0, 100), 250))
 
-        time.sleep(1)
-        eyes_open_thread = threading.Thread(target=self.cosmetics_movement.open_eyes, args=(1,))
-        eyes_open_thread.start()
-
-        time.sleep(5)
-        head_thread = threading.Thread(target=self.joints_movement.nod, args=(2,2))
-        head_thread.start()
-
-        time.sleep(6)
-        led_thread_1 = threading.Thread(target=self.led_controller.toggle_led_sections, args=(7, (255, 0, 0),(0, 255, 0),(0, 0, 255), 250))
-        led_thread_1.start()
-
-        time.sleep(1)
-        led_thread_1 = threading.Thread(target=self.led_controller.toggle_led_sections, args=(7, (255, 0, 0),(0, 0, 0),(0, 0, 0), 250))
-        led_thread_1.start()
-
-        time.sleep(1)
-        led_thread_1 = threading.Thread(target=self.led_controller.toggle_led_sections, args=(7, (0, 0, 0),(0, 255, 0),(0, 0, 0), 250))
-        led_thread_1.start()
-
-        time.sleep(1)
-        led_thread_1 = threading.Thread(target=self.led_controller.toggle_led_sections, args=(7, (0, 0, 0),(0, 0, 0),(0, 0, 255), 250))
-        led_thread_1.start()
-
-        time.sleep(1)
-        led_thread_1 = threading.Thread(target=self.led_controller.toggle_led_sections, args=(7, (100, 0, 0),(0, 100, 0),(0, 0, 100), 250))
-        led_thread_1.start()
-
-        head_thread.join()
-        led_thread_1.join()
-        eyes_thread.join()
-        eyes_open_thread.join()
+        # Wait for the first set of actions to finish
         play_thread.join()
+        for timer in self.timers:
+            timer.join()
         time.sleep(5)
 
         self.state = "relax_complete"
@@ -294,17 +249,26 @@ class RelaxBehavior:
         play_thread = threading.Thread(target=play_audio, args=(audio_file,))
         play_thread.start()
 
-        # Mimic cute animal movements (different from the previous set) - 3 movements only, affirming relaxation
-        threading.Timer(1, lambda: threading.Thread(target=self.led_controller.fade_in_led, args=(2, (0, 255, 0), 150)).start()).start()
-        threading.Timer(5, lambda: threading.Thread(target=self.led_controller.fade_out_led, args=(2,)).start()).start()
+        # Schedule all movements
+        self.add_timer(1, self.led_controller.fade_in_led, (2, (0, 255, 0), 150))
+        self.add_timer(5, self.led_controller.fade_out_led, (2,))
+        self.add_timer(4, self.cosmetics_movement.eye_wink, (1, 1, "left"))
+        self.add_timer(4, self.cosmetics_movement.eye_wink, (1, 1, "right"))
+        self.add_timer(6, self.cosmetics_movement.wagging_tail, (2, 3))
 
-        threading.Timer(4, lambda: threading.Thread(target=self.cosmetics_movement.eye_wink, args=(1, 1, "left")).start()).start()
-        threading.Timer(4, lambda: threading.Thread(target=self.cosmetics_movement.eye_wink, args=(1, 1, "right")).start()).start()
-
-        threading.Timer(6, lambda: threading.Thread(target=self.cosmetics_movement.wagging_tail, args=(2, 3)).start()).start()
-
+        # Wait for the first set of actions to finish
         play_thread.join()
+        for timer in self.timers:
+            timer.join()
+
         self.state = "complete"
+    
+    def exit(self):
+        #play_thread.join()
+
+        exit_behaviour_thread = threading.Thread(target=self.audio_player.play_audio, args=('mp3_files/i_will_stop.mp3',))
+        exit_behaviour_thread.start()
+        exit_behaviour_thread.join()
 
 
 
